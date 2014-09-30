6805f2aafa728247a1ec9d665e454d76a562662a
Will Tipton <wwt26@cornell.edu>
2014-09-29 23:32:26 -0700

    Initial commit: LR model with batch gradient descent, the Graph Laplacian regularizer, save and restore, and an arbitrary number of features.

---
+++ LRModel.cpp
@@ -0,0 +1,304 @@
+/*
+ * LRModel.cpp
+ *
+ *  Created on: Sep 21, 2014
+ *      Author: wtipton
+ */
+
+#include "LRModel.h"
+
+#include <cmath>
+#include <iostream>
+#include <zlib.h>
+
+LRModel::LRModel(int n, int t, std::vector<std::vector<double>> data, double alpha, double lambda, double e)
+: n_(n), t_(t), div_len_(1.0/t), num_lattice_points_(pow(t+1, n)) {
+	for (int i = 0; i < num_lattice_points_; i++)
+		b_.push_back(1.0);
+
+	initAdjLatticePoints();
+
+	trainModel(data, alpha, lambda, e);
+}
+
+LRModel::LRModel(const std::string& filename) {
+	gzFile inFile = gzopen(filename.c_str(), "rb");
+	if (inFile == Z_NULL)
+		std::cout << "ERROR: LRModel could not open file " << filename << std::endl;
+
+	if (gzread (inFile, (char*)(&n_), sizeof (int)) < (int) sizeof(int))
+		std::cout << "ERROR: gzread failed in LRModel reading n_" << std::endl;
+	if (gzread (inFile, (char*)(&t_), sizeof (int)) < (int) sizeof(int))
+		std::cout << "ERROR: gzread failed in LRModel reading t_" << std::endl;
+	if (gzread (inFile, (char*)(&div_len_), sizeof (double)) < (int) sizeof(double))
+		std::cout << "ERROR: gzread failed in LRModel reading div_len_" << std::endl;
+	if (gzread (inFile, (char*)(&num_lattice_points_), sizeof (int)) < (int) sizeof(int))
+		std::cout << "ERROR: gzread failed in LRModel reading num_lattice_points_" << std::endl;
+
+	double b;
+	for (int i = 0; i < num_lattice_points_; i++) {
+		if (gzread (inFile, (char*)(&b), sizeof (double)) < (int) sizeof(double))
+			std::cout << "ERROR: gzread failed in LRModel reading b_" << std::endl;
+		b_.push_back(b);
+	}
+
+	gzclose(inFile);
+
+	initAdjLatticePoints();
+}
+
+void LRModel::writeToDisk(const std::string& filename) {
+	gzFile outFile = gzopen(filename.c_str(), "wb");
+	if (outFile == Z_NULL)
+		std::cout << "ERROR: gzwrite failed to open " << filename << std::endl;
+
+	// Save: n, t, div_len_, num_lattice_points_, and b. Will need to re-calculate adj_lattice_pts_ upon reading.
+
+	if (! gzwrite (outFile, (char*)(&n_), sizeof (int)))
+		std::cout << "ERROR: gzwrite failed writing n_ in LRModel:writeToDisk" << std::endl;
+	if (! gzwrite (outFile, (char*)(&t_), sizeof (int)))
+		std::cout << "ERROR: gzwrite failed writing t_ in LRModel:writeToDisk" << std::endl;
+	if (! gzwrite (outFile, (char*)(&div_len_), sizeof (double)))
+		std::cout << "ERROR: gzwrite failed writing div_len_ in LRModel:writeToDisk" << std::endl;
+	if (! gzwrite (outFile, (char*)(&num_lattice_points_), sizeof (int)))
+		std::cout << "ERROR: gzwrite failed writing num_lattice_points_ in LRModel:writeToDisk" << std::endl;
+
+	for (int i = 0; i < num_lattice_points_; i++)
+		if (! gzwrite (outFile, (char*)(&(b_[i])), sizeof(double)))
+			std::cout << "ERROR: gzwrite failed in LRModel:writeToDisk" << std::endl;
+
+	gzclose(outFile);
+}
+
+LRModel::~LRModel() {
+	// nothing to do here
+}
+
+
+void LRModel::initAdjLatticePoints() {
+	for (int i_point = 0; i_point < num_lattice_points_; i_point++) {
+		std::vector<int> adj_pts; // will hold indices of points adjacent to point i_point
+
+		// Get the coords of i_point.
+		int x[n_];
+		indxToCoords(i_point, x);
+
+		// Initialize delta to all -1's.  (x+delta) is a candidate adjacent point, where each
+		// entry of delta is -1, 0, or 1.  We just need to check that delta isn't all 0's nor is it
+		// out of bounds.  And, we'll iterate through all possible delta vectors, essentially, counting
+		// in base 3 where the three possible digits are -1, 0, and 1.
+		int delta[n_];
+		for (int i = 0; i < n_; i++)
+			delta[i] = -1;
+
+		// the following loop does this, essentially:
+		/*
+		while (1) {
+			if (delta is not all 0s && x+delta is in bounds) {
+				add x+delta to adj_pts;
+			}
+			if (delta is all 1s)
+				break;
+			increment delta;
+		}
+		*/
+
+		while (1) {
+			// check if delta is all zeros
+			bool delta_is_all_zeros = true;
+			for (int i = 0; i < n_; i++)
+				if (delta[i] != 0)
+					delta_is_all_zeros = false;
+
+			// calculate x+delta
+			int x_plus_delta[n_];
+			for (int i = 0; i < n_; i++)
+				x_plus_delta[i] = x[i] + delta[i];
+
+			// check if x+delta is in bounds
+			bool x_plus_delta_is_in_bounds = true;
+			for (int i = 0; i < n_; i++)
+				if (x_plus_delta[i] < 0 || x_plus_delta[i] >= t_+1)
+					x_plus_delta_is_in_bounds = false;
+
+			// add x+delta to list of adjacent points if appropriate
+			if (!delta_is_all_zeros && x_plus_delta_is_in_bounds) {
+				adj_pts.push_back(coordsToIndx(x_plus_delta));
+			}
+
+			// if delta is all ones, we're done
+			bool delta_is_all_ones = true;
+			for (int i = 0; i < n_; i++)
+				if (delta[i] != 1)
+					delta_is_all_ones = false;
+			if (delta_is_all_ones)
+				break;
+
+			// increment delta
+			int i = 0;
+			while (i < n_ && delta[i] == 1) {
+				delta[i] = -1;
+				i++;
+			}
+			delta[i]++;
+		}
+
+		adj_lattice_pts_.push_back(adj_pts);
+	}
+
+/*	// print adjacent lattice points
+	for (int n = 0; n < num_lattice_points_; n++) {
+		std::cout << "adj to " << n << ":";
+		for (int k=0; k<adj_lattice_pts_[n].size(); k++)
+			std::cout << " " << adj_lattice_pts_[n][k];
+		std::cout << std::endl;
+	}*/
+}
+
+
+void LRModel::trainModel(std::vector<std::vector<double>> data, double alpha, double lambda, double e) {
+	std::vector<std::map<int,double>> weights; // weights of training examples
+	std::vector<std::vector<int>> adjLatticeToData; // essentially a map from lattice points to adjacent training examples
+	std::vector<std::vector<int>> adjDataToLattice; // essentially a map from training examples to adjacent lattice points
+	// initialize weights and adjacency structures
+	makeTrainingWeightsAndAdj(data, weights, adjDataToLattice, adjLatticeToData);
+
+	// batch gradient descent
+	while (1) {
+		double norm_of_grad = 0.0;
+		for (int k = 0; k < num_lattice_points_; k++) {
+			double dJdbk = 0.0;
+			// calculate the derivative of J wrt b_k
+			for (int i : adjLatticeToData[k]) { // here, i is an index into the data vector of an example that is adjacent to lattice point k
+				double prediction_i = 0.0;
+				for (int j : adjDataToLattice[i]) // for lattice points j adjacent to i
+					prediction_i += b_[j] * weights[i][j];
+				dJdbk += (prediction_i - data[i][0]) * weights[i][k] / data.size();
+			}
+			for (int n : adj_lattice_pts_[k]) { // regularization: for nodes n adjacent to k
+				dJdbk -= lambda * 2 * (b_[n] - b_[k]);
+			}
+			b_[k] -= alpha * dJdbk;
+			norm_of_grad += dJdbk*dJdbk;
+		}
+		norm_of_grad = sqrt(norm_of_grad);
+		std::cout << norm_of_grad << std::endl;
+		if (norm_of_grad <= e)
+			break;
+	}
+
+/*	// Print learned values at each lattice point.
+	for (int k = 0; k < num_lattice_points_; k++)
+		std::cout << b_[k] << std::endl;
+*/
+}
+
+void LRModel::makeTrainingWeightsAndAdj(std::vector<std::vector<double>> data, std::vector<std::map<int,double>>& weights,
+		                                std::vector<std::vector<int>>& adjDataToLattice, std::vector<std::vector<int>>& adjLatticeToData) {
+	weights.clear();
+	adjDataToLattice.clear();
+	adjLatticeToData.clear();
+	for (size_t i = 0; i < data.size(); i++) {
+		weights.push_back(std::map<int,double>());
+	}
+	for (int i = 0; i < num_lattice_points_; i++) {
+		adjLatticeToData.push_back(std::vector<int>());
+	}
+
+	for (size_t i = 0; i < data.size(); i++) {
+		std::map<int,double> a = getWeights(&(data[i][1]));
+		std::vector<int> adj_pts;
+		for (std::pair<int,double> p : a) {
+			int lat_pt_indx = p.first;
+			double lat_pt_weight = p.second;
+
+			adjLatticeToData[lat_pt_indx].push_back(i);
+			weights[i][lat_pt_indx] = lat_pt_weight;
+			adj_pts.push_back(lat_pt_indx);
+		}
+		adjDataToLattice.push_back(adj_pts);
+	}
+}
+
+std::map<int,double> LRModel::getWeights(double x[]) {
+	std::map<int,double> result;
+
+	// Find the  each dimension, there's ind the upper and lower bounding tick marks for x in each dimension
+	int low[n_], high[n_];
+	for (int i = 0; i < n_; i++) {
+		low[i] = floor(x[i]*t_);
+		high[i] = ceil(x[i]*t_);
+	}
+
+	// Avoid the degenerate case where a point is right on a lattice point, because that'd lead to a div-by-0 later.
+	for (int i = 0; i < n_; i++) {
+		if (low[i] == high[i]) {
+			if (low[i] == 0)
+				high[i]++;
+			else
+				low[i]--;
+		}
+	}
+
+	// convert from lattice to cartesian
+	double d_low[n_], d_high[n_];
+	for (int i = 0; i < n_; i++) {
+		d_low[i] = div_len_ * low[i];
+		d_high[i] = div_len_ * high[i];
+	}
+
+	// Calculate weights for adjacent points.
+	// Denom is the total area of one grid box.
+	double denom = 1.0;
+	for (int i = 0; i < n_; i++)
+		denom *= (d_high[i] - d_low[i]);
+
+	// A little bit of magic here to loop over all lattice points adjacent to x.  So there's one such lattice point
+	// for each number from 0 up to (2^n_)-1.  Think of it in binary, and the ith bit controls whether we use the
+	// high or low tick mark in the ith dimension.
+	for (int i = 0; i < (1<<n_); i++) {
+		int c[n_];
+		int ci = i;
+		double area = 1.0;
+		for (int j = 0; j < n_; j++) {
+			if (ci % 2) {
+				c[j] = low[j];
+				area *= d_high[j] - x[j];
+			} else {
+				c[j] = high[j];
+				area *= x[j] - d_low[j];
+			}
+			ci /= 2;
+		}
+		result[coordsToIndx(c)] = area / denom;
+	}
+
+	return result;
+}
+
+double LRModel::predict(double x[]) {
+	std::map<int,double> a = getWeights(x);
+
+	double result = 0.0;
+	for (auto p : a) {
+		result += p.second * b_[p.first];
+	}
+
+	return result;
+}
+
+int LRModel::coordsToIndx(const int c[]) {
+	int result = c[0];
+	for (int i = 1; i < n_; i++) {
+		result *= t_+1;
+		result += c[i];
+	}
+	return result;
+}
+
+void LRModel::indxToCoords(int indx, int c[]) {
+	for (int i = n_-1; i >=0; i--) {
+		c[i] = indx % (t_+1);
+		indx /= (t_+1);
+	}
+}
---
+++ LRModel.h
@@ -0,0 +1,100 @@
+/*
+ * LRModel.h
+ *
+ * Lattice Regression model.
+ *
+ * The big idea is pretty straightforward.  We want to model some function.  We throw down a grid (i.e. a lattice)
+ * over the space of possible inputs and learn something like the values of the function at those points from
+ * some training data.  We store those values and use them to predict the value of the function for new inputs
+ * via an interpolation scheme.  For more info, see, e.g.:
+ *
+ * http://www.mayagupta.org/publications/GarciaAroraGupta_lattice_regression_IEEETransImageProcessing2012.pdf
+ *
+ * Uses batch gradient descent and a regularizer that penalizes differences between adjacent lattice points.
+ * Assumes features (i.e. inputs) are normalized to fall in [0,1].  The response variable (i.e. output) need not
+ * be normalized.
+ *
+ * Created on: Sep 21, 2014
+ *     Author: wtipton
+ */
+
+#ifndef LRMODEL_H_
+#define LRMODEL_H_
+
+#include <string>
+#include <vector>
+#include <array>
+#include <map>
+
+class LRModel {
+public:
+	// Constructor for training a new model.
+	// Inputs:
+	//  n: number of features, i.e. dimensions.
+	//  t: number of intervals per dimension. So, the number of lattice points per dimension is (t_+1).
+	//  data: vector of vectors of dimensions [m][n+1]. Each data[i][0] is the value of the response variable
+	//        associated with features data[i][1 thru n].
+	//  alpha: gradient descent step size parameter
+	//  lambda: regularization parameter
+	//  e: convergence parameter: stop when the norm of the gradient falls below this value.
+	LRModel(int n, int t, std::vector<std::vector<double>> data, double alpha, double lambda, double e);
+
+	// Constructor for reading saved model from disk.
+	LRModel(const std::string& filename);
+
+	virtual ~LRModel();
+
+	// Save the model in a serialized format on disk.
+	void writeToDisk(const std::string& filename);
+
+	// Returns predicted response given some features. The input array should have length n.
+	double predict(double features[]);
+
+private:
+	// Number of features
+	int n_;
+
+	// Number of divisions per dimension. So, the number of lattice points per dimension is (t_+1).
+	int t_;
+
+	// Length of a division, i.e. 1.0/t.
+	double div_len_;
+
+	// Number of lattice points, (t_+1)^n_
+	int num_lattice_points_;
+
+	// Helper function called by both constructors to initialize adj_lattice_pts_ which holds indices
+	// of lattice points adjacent to each lattice point. In particular, adj_lattice_pts_[i] will be a
+	// vector of all points adjacent to point i.
+	void initAdjLatticePoints();
+	std::vector<std::vector<int>> adj_lattice_pts_;
+
+	// Values of surface at lattice points.
+	std::vector<double> b_;
+
+	// m is the number of training examples.  Other inputs are as to the constructor.
+	void trainModel(std::vector<std::vector<double>> data, double alpha, double lambda, double e);
+
+	// Initialize supplementary weights and adj structures.
+	// Inputs:
+	//   m: number of training examples
+	//   data: m-by-n+1 array as in constructor
+	// Outputs:
+	//   weights: vector of length data.size() containing maps from lattice point index to weight for each training example
+	//   adjDataToLattice: vector of length data.size() containing arrays of lattice points adjacent to each training example
+	//   adjLatticeToData: vector (of length num_lattice_points_) of vectors containing indices (into data) of examples adjacent
+	//                     to each lattice point
+	void makeTrainingWeightsAndAdj(std::vector<std::vector<double>> data, std::vector<std::map<int,double>>& weights, std::vector<std::vector<int>>& adjDataToLattice, std::vector<std::vector<int>>& adjLatticeToData);
+
+	// Get non-zero weights (and corresponding adjacent lattice points) for a given set of inputs.
+	std::map<int,double> getWeights(double x[]);
+
+	// Takes an int array of coordinates of length m_ describing a grid point and returns the corresponding index into b_.
+	// NB: The "coordinates" here are integers from 0 to t_.  Multiply them by div_len_ to convert to cartesian.
+	int coordsToIndx(const int c[]);
+
+	// Takes an index into b_ and writes an int array of coordinates.
+	void indxToCoords(int indx, int c[]);
+};
+
+#endif /* LRMODEL_H_ */
